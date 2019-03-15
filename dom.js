dafa2086b559c4788c5a57f0a80eee0f1f2b632a
harshaathnere <harshaathnere@gmail.com>
2019-03-15 12:48:02 +0530

    added removed function

---
+++ dom.js
@@ -0,0 +1,29 @@
+function sumValues() {
+    var n1 = Number(document.getElementById("n1").value);
+    var n2 = Number(document.getElementById("n2").value);
+
+}
+function Multiply()
+{
+    sumValues();
+    result = n1*n2;
+    alert("the ans is" +result);
+}
+function Substraction()
+{
+    sumValues();
+    result = n1-n2;
+    alert("the ans is" +result);
+}
+function Addition()
+{
+    sumValues();
+    result = n1+n2;
+    alert("the ans is" +result);
+}
+function Division()
+{
+    sumValues();
+    result = n1/n2;
+    alert("the ans is" +result);
+}
