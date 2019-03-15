b34de8a36156a035a0572ac444bb1d88911a0a7c
harshaathnere <harshaathnere@gmail.com>
2019-03-15 14:15:20 +0530

    added removed function

--- dom.js
+++ dom.js
@@ -1,31 +1,28 @@
 var n1 , n2;
 function sumValues() {
     n1 = Number(document.getElementById("n1").value);
-     n2 = Number(document.getElementById("n2").value);
-   return {n1,n2}
+    n2 = Number(document.getElementById("n2").value);
+   
 }
 function Multiply()
 {
-     
+     sumValues()
     result = n1*n2;
     alert("the ans is" + result);
 }
 function Substraction()
-{
-     
+{   sumValues()
     result = n1-n2;
     alert("the ans is" + result);
 }
 function Addition()
     {
-     
+        sumValues()
     result = n1+n2;
     alert("the ans is" + result);
 }
 function Division()
-{
-
-     
+{  sumValues()
     result = n1/n2;
     alert("the ans is" + result);
 }
